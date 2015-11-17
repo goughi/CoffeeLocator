eb7f35ec0da025f618fc39fa422829c63a1c799c
goughi <goughi@hotmail.com>
2015-11-17 12:36:52 +0000

    hi

---
+++ CoffeeApp/Controllers/CoffeeStoresController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeApp.Models;
+
+namespace CoffeeApp.Controllers
+{
+    public class CoffeeStoresController : Controller
+    {
+        private DrinkContext db = new DrinkContext();
+
+        // GET: CoffeeStores
+        public ActionResult Index()
+        {
+            return View(db.CoffeeStores.ToList());
+        }
+
+        // GET: CoffeeStores/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CoffeeStore coffeeStore = db.CoffeeStores.Find(id);
+            if (coffeeStore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(coffeeStore);
+        }
+
+        // GET: CoffeeStores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CoffeeStores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Eircode,StoreName,Location,IsOpen,StoreRating")] CoffeeStore coffeeStore)
+        {
+            if (ModelState.IsValid)
+            {
+                try {
+                    db.CoffeeStores.Add(coffeeStore);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch(Exception e)
+                { Console.WriteLine(e.ToString()); }
+            }
+
+            return View(coffeeStore);
+        }
+
+        // GET: CoffeeStores/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CoffeeStore coffeeStore = db.CoffeeStores.Find(id);
+            if (coffeeStore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(coffeeStore);
+        }
+
+        // POST: CoffeeStores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Eircode,StoreName,Location,IsOpen,StoreRating")] CoffeeStore coffeeStore)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(coffeeStore).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(coffeeStore);
+        }
+
+        // GET: CoffeeStores/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CoffeeStore coffeeStore = db.CoffeeStores.Find(id);
+            if (coffeeStore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(coffeeStore);
+        }
+
+        // POST: CoffeeStores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            CoffeeStore coffeeStore = db.CoffeeStores.Find(id);
+            db.CoffeeStores.Remove(coffeeStore);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CoffeeApp/Models/CoffeeStore.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace CoffeeApp.Models
+{
+    public enum Rating { 1, 2, 3, 4, 5 }
+    public enum DrinkSize { tall, grande, vendi }
+    public class CoffeeStore
+    {
+        public String StoreName { get; set; }
+        [Key]
+        public String Eircode { get; set; }
+
+        public String Location { get; set; }
+        public bool IsOpen { get; set; }
+        [Range(0,5,ErrorMessage ="rating must be between 1 and 5")]
+        public double StoreRating { get; set; }
+
+        public virtual ICollection<Drink> Drinks { get; set; }
+    }
+    public class Drink
+    {
+        [Key]
+        public int DrinkID { get; set; }
+        public String DrinkName { get; set; }
+        public DrinkSize DrinkSize { get; set; }
+        public double Price { get; set; }
+
+        public String Eircode { get; set; }
+        public virtual CoffeeStore CoffeeStore { get; set; }
+
+    }
+
+    public class DrinkContext : DbContext
+    {
+        public DrinkContext() : base("DefaultConnection")
+        {
+            Database.SetInitializer<DrinkContext>(new DropCreateDatabaseIfModelChanges<DrinkContext>());
+        }
+        public DbSet<CoffeeStore> CoffeeStores { get; set; }
+        public DbSet<Drink> Drinks { get; set; }
+    }
+
+    public class DrinkRepository
+    {
+        public void AddStore(CoffeeStore cs)
+        {
+            using (DrinkContext db = new DrinkContext())
+            {
+                try
+                {
+                    db.CoffeeStores.Add(cs);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                { Console.WriteLine(e.ToString()); }
+            }
+        }
+
+       static void AddStore(Drink drink)
+        {
+            using (DrinkContext db = new DrinkContext())
+            {
+                try
+                {
+                    db.Drinks.Add(drink);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                { Console.WriteLine(e.ToString()); }
+            }
+        }
+    }
+    public class CodeFirstTest
+    {
+        static void main()
+        {
+            DrinkRepository repository = new DrinkRepository();
+            CoffeeStore st1 = new CoffeeStore() { Eircode = "C15C98E", Location = "O' Connell St. Limerick", IsOpen = true, StoreName = "Starbucks", StoreRating = 0 };
+            repository.AddStore(st1);
+        }
+    }
+}
